b9d7cff12c507975f7823ebed578fc657cb42c48
Misael-GC <misael.gomez1623@gmail.com>
2022-08-20 08:57:29 -0500

    forms tipografía y aspect ratio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/index.html", "./src/**/*.{html,js}"],
+  purge:[],
+  darkMode:false,
   theme: {
     extend: {},
+  },
+  variants:{
+    extend:{},
   },
   plugins: [],
 }
